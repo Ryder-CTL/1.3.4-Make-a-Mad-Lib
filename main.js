9d5f9f318a0546db19f5fa94cfe6301a835fd1e8
Ryder-CTL <bunnyjryder@gmail.com>
2026-05-20 15:06:24 -0700

    Implement madlib creation and console logging
    Add madlib generation and log output to console.

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ var adjective2 = prompt('Provide another adjective.');
 var adverb = prompt('Provide an adverb.');
 
 // Write your code below
+var Madlib = "The dog was" + adjective1 + "and" + adjective2 + "while it ate" + adverb + "in the kitchen.";
+consol.log(Madlib);
